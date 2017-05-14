910a61b85174b86e8af0a12a354d5533389d190a
LizHuang <search.psop@gmail.com.tw>
2017-05-14 20:40:54 +1000

    Add image uploading with paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
-
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all.order("created_at DESC")
@@ -10,14 +10,14 @@ class PinsController < ApplicationController
   end
 
   def new
-	@pin = current_user.pins.build
+    @pin = current_user.pins.build
   end
 
   def create
-  	@pin = current_user.pins.build(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     if @pin.save
-      redirect_to @pin, notice: "Pin was successfully create"
+      redirect_to @pin, notice: "Pin was successfully created"
     else
       render 'new'
     end
@@ -39,13 +39,19 @@ class PinsController < ApplicationController
     redirect_to root_path
   end
 
+  def upvote
+    @pin.upvote_by current_user
+    redirect_to :back
+  end
+
   private
 
   def pin_params
-	params.require(:pin).permit(:title, :description)
+    params.require(:pin).permit(:title, :description, :image)
   end
 
   def find_pin
     @pin = Pin.find(params[:id])
   end
+
 end
